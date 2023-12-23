0ce81d9f79d493b21eeddad35a11639342925029
ivanmorikov <ivanmorikov48@gmail.com>
2023-12-23 20:11:32 +0300

    modified cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"concert_pre-poster/pkg/store/sqlstore"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"golang.org/x/text/encoding/charmap"
 )
 
 func main() {
-	fmt.Print("hello")
+	fmt.Println("hello")
+	_, err := sqlstore.NewClient("ToDelete", "postgres", "Tylpa31")
+	if err != nil {
+		fmt.Println(wrapErrorFromDB(err))
+	}
+}
+
+func startServer() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -14,5 +23,12 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080
+}
 
+func wrapErrorFromDB(err error) error {
+	if err == nil {
+		return err
+	}
+	utf8Text, _ := charmap.Windows1251.NewDecoder().String(err.Error())
+	return fmt.Errorf(utf8Text)
 }
